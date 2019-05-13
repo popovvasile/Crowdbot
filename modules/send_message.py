3a6ff2ee49258a016d36865423eb02e4a486b544
Vasile <popovvasile@gmail.com>
2019-05-13 18:09:49 +0200

    anonymous forwording

--- modules/send_message.py
+++ modules/send_message.py
@@ -93,46 +93,43 @@ class SendMessageToUsers(object):
         chat_id, txt = initiate_chat_id(update)
         chats = chats_table.find({"bot_id": bot.id})
         for chat in chats:
-            bot.forward_message(chat_id=chat["chat_id"],
-                                from_chat_id=chat_id,
-                                message_id=update.message.message_id)
             if update.message.text:
                 bot.send_message(chat["chat_id"], update.message.text)
 
             elif update.message.photo:
-                photo_file = update.message.photo
-                bot.send_photo(chat["chat_id"], photo_file)
+                photo_file = update.message.photo[0].get_file().file_id
+                bot.send_photo(chat_id=chat["chat_id"], photo=photo_file)
 
             elif update.message.audio:
-                audio_file = update.message.audio
+                audio_file = update.message.audio.get_file().file_id
                 bot.send_audio(chat["chat_id"], audio_file)
 
             elif update.message.voice:
-                voice_file = update.message.voice
-                bot.send_voice((chat["chat_id"], voice_file))
+                voice_file = update.message.voice.get_file().file_id
+                bot.send_voice(chat["chat_id"], voice_file)
 
             elif update.message.document:
-                document_file = update.message.document
+                document_file = update.message.document.get_file().file_id
                 bot.send_document(chat["chat_id"], document_file)
 
             elif update.message.sticker:
-                sticker_file = update.message.sticker
+                sticker_file = update.message.sticker.get_file().file_id
                 bot.send_sticker(chat["chat_id"], sticker_file)
 
             elif update.message.game:
-                sticker_file = update.message.game
+                sticker_file = update.message.game.get_file().file_id
                 bot.send_game(chat["chat_id"], sticker_file)
 
             elif update.message.animation:
-                animation_file = update.message.animation
+                animation_file = update.message.animation.get_file().file_id
                 bot.send_animation(chat["chat_id"], animation_file)
 
             elif update.message.video:
-                video_file = update.message.video
+                video_file = update.message.video.get_file().file_id
                 bot.send_video(chat["chat_id"], video_file)
 
             elif update.message.video_note:
-                video_note_file = update.message.audio
+                video_note_file = update.message.audio.get_file().file_id
                 bot.send_video_note(chat["chat_id"], video_note_file)
 
         buttons = list()
