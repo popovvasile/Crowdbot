11f05d495ca4f8fed69ea7f04a6da7c141736154
Vasile Pop <vasile.popa@perseus.de>
2020-06-01 23:10:14 +0200

    splitting deploy test and prod

---
+++ deploy_production.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env sh
+set -e
+#cd ..
+source venv/bin/activate
+export SHOP_PRODUCTION=1
+export BOTFATHER_TOKEN=892372248:AAEM32Ye0ye4rTkyTuk5Z4minhsNKVhSQ-w
+cd crowdrobot
+killall python
+nohup python crowdrobot_alive_checker.py &
+nohup python rest_api/crowdbot_rest_api.py &
+cd ..
+cd botfather
+nohup python runner.py &
--- deploy_testing.sh
+++ deploy_testing.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/env sh
 set -e
-
-export SHOP_PRODUCTION=1
+#cd ..
+source venv/bin/activate
+export SHOP_PRODUCTION=0
 export BOTFATHER_TOKEN=892372248:AAEM32Ye0ye4rTkyTuk5Z4minhsNKVhSQ-w
 cd crowdrobot
 killall python
 nohup python crowdrobot_alive_checker.py &
 nohup python rest_api/crowdbot_rest_api.py &
+nohup python rest_api/face.py &
 cd ..
 cd botfather
 nohup python runner.py &
