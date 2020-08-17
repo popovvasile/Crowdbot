bde4be6a5475b604887eabcc2351d2b165011918
Vasile Pop <vasile.popa@perseus.de>
2020-08-17 07:40:07 +0200

    testing

--- start_api.py
+++ start_api.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from flask import Flask
 from flask_restful import Api
@@ -33,6 +34,12 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host="127.0.0.1",
-            port=8000,
-            debug=True)
+    if os.environ['SHOP_PRODUCTION'] == "1":
+        app.run(host="127.0.0.1",
+                port=8000,
+                debug=True)
+    else:
+        app.run(host="127.0.0.1",
+                port=8001,
+                debug=True)
+
